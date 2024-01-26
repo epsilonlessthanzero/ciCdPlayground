4ff9611392529389a2fb85fd9c2c752262913c97
epsilonlessthanzero <150372412+epsilonlessthanzero@users.noreply.github.com>
2024-01-26 09:26:27 +0100

    Customize main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Simon Cordes',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Automated workflow to continuously working on and releasing changes.'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'AWS Codepipeline, maybe?',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'How to set up a CI/CD workflow/automation.'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'hiking',
+			'watching movies',
+			'cooking'
 		]
 	}
 });
